1eeba1a75dd2f8cdcb96e28ef22f67d58fd2257f
Peter Washington <peter@wmkagency.com>
2020-09-04 23:21:33 -0600

    added data as json

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,52 +1,52 @@
-import Theme from "./src/vars/ThemeOptions";
+// import Theme from "./src/vars/ThemeOptions";
 
-const path = require(`path`);
+// const path = require(`path`);
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions;
-  createRedirect({
-    fromPath: "/",
-    toPath: `${Theme.paths.home}`,
-    isPermanent: true,
-    redirectInBrowser: true
-  });
-  const pageTemplate = path.resolve("./src/node/page.js");
-  createRedirect({
-    fromPath: "/home",
-    toPath: `/`,
-    isPermanent: true,
-    redirectInBrowser: true
-  });
-  createRedirect({
-    fromPath: `${Theme.paths.home}`,
-    toPath: `/`,
-    isPermanent: true,
-    redirectInBrowser: true
-  });
-  return graphql(`
-    {
-      pages: allContentfulPages {
-        edges {
-          node {
-            slug
-            title
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      throw result.errors;
-    }
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage, createRedirect } = actions;
+//   createRedirect({
+//     fromPath: "/",
+//     toPath: `${Theme.paths.home}`,
+//     isPermanent: true,
+//     redirectInBrowser: true
+//   });
+//   const pageTemplate = path.resolve("./src/node/page.js");
+//   createRedirect({
+//     fromPath: "/home",
+//     toPath: `/`,
+//     isPermanent: true,
+//     redirectInBrowser: true
+//   });
+//   createRedirect({
+//     fromPath: `${Theme.paths.home}`,
+//     toPath: `/`,
+//     isPermanent: true,
+//     redirectInBrowser: true
+//   });
+//   return graphql(`
+//     {
+//       pages: allContentfulPages {
+//         edges {
+//           node {
+//             slug
+//             title
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       throw result.errors;
+//     }
 
-    // Create site pages
-    result.data.pages.edges.forEach(edge => {
-      createPage({
-        // Path for this page — required
-        path: `${edge.node.slug}`,
-        component: pageTemplate,
-        context: edge.node
-      });
-    });
-  });
-};
+//     // Create site pages
+//     result.data.pages.edges.forEach(edge => {
+//       createPage({
+//         // Path for this page — required
+//         path: `${edge.node.slug}`,
+//         component: pageTemplate,
+//         context: edge.node
+//       });
+//     });
+//   });
+// };
