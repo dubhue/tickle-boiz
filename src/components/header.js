d93c6d68d485da55d75a8bebbd2469d1cc75bfec
Peter Washington <peter@wmkagency.com>
2021-08-13 08:18:34 -0600

    title change

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,39 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+      marginBottom: `1.45rem`
+    }}>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+        padding: `1.45rem 1.0875rem`
+      }}>
+      <h6 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+            textDecoration: `none`
+          }}>
+          TICKLE BOIZ {/* {siteTitle} */}
         </Link>
-      </h1>
+      </h6>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: ``
+};
 
-export default Header
+export default Header;
